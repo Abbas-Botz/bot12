7a0a25ad0291b63952acedeab1cbfd9bf4589e82
Abbas-Botz <92032706+Abbas-Botz@users.noreply.github.com>
2021-12-07 07:52:22 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,27 +1,27 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT BY ABBAS OFC
 
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ 8881 0899 5820 020
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ 3901 0899 5820 020
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ 881 0899 5820 020
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ 852 899 5820 020
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 0899 5820 020
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0899 5820 020
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0899 5820 020
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama A**as
 
 *📲 PULSA*
 
